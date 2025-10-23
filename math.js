429e3bee0d23c6fa2c6f3be7aff90605f70a4ef4
Abin vk <vettikkadanabin@gmail.com>
2025-10-23 21:12:24 +0530

    javascript

--- math.js
+++ math.js
@@ -4,6 +4,14 @@ console.log(Math.round(4.5));
 console.log(Math.round(4.3));
 console.log(Math.round(4.7));
 console.log(Math.round(5.95), Math.round(5.5), Math.round(5.05));
-
 console.log(8+2);
-// end of code //
+
+// VARIABLES//
+
+let variables = 3;
+console.log(variables);
+
+
+let calculation = 5+5;
+console.log(calculation);
+console.log(calculation + 10);
