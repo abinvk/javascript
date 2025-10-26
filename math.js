7808905b0b26171e6ebc2527ba355a0d5bffb254
Abin vk <vettikkadanabin@gmail.com>
2025-10-26 19:38:55 +0530

    javascript

--- math.js
+++ math.js
@@ -29,3 +29,31 @@ variable1 = variable1 + 3;
 console.log(variable1);
 console.log(typeof variable1);
 console.log(typeof massege);
+
+  
+//Create a <script> element. Inside the <script>, create a variable called 'name', and save your name in this variable (as a string)//
+//Answer//
+let name = "Abin";
+console.log(name);
+
+// Continuing from 5a, display the message 'My name is: $(name)' in the console (insert the 'name' variable created in 5a into this message)//
+//Answer//
+
+console.log('My name is:'  + (name));
+
+// At a restaurant, you order 1 coffee ($5), 2 bagels ($3 each), and 1 soup ($9). Calculate the cost and save it in a variable called 'cost'//
+//Answer//
+
+let cost = 5 + (2 * 3) + 9;
+console.log(cost);
+
+ //Continuing from 5c, display 'Cost of food: $${cost}' in the console.//
+// Answer//
+
+console.log('cost of food: $' + cost);
+
+//Let's say the restaurant charges a 10% tax. Using the 'cost' variable from 5c, calculate the tax (hint: multiply by 0.1), and save the result in a variable.//
+//Answer//
+
+let tax = cost * 0.1;
+console.log('Tax: $' + tax);
