86fc531cf73c8c8df16d890941989311674457ba
Abin vk <vettikkadanabin@gmail.com>
2025-12-07 21:32:06 +0530

    javascript

--- todo-list.js
+++ todo-list.js
@@ -1,4 +1,22 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes'];
+
+renderTdoList();
+
+function renderTdoList () {
+
+let todoListHTML = '';
+for(let i = 0; i<todoList.length; i++){
+    const todo =todoList[i];
+    const html = `<p>${todo}</P>`;
+    todoListHTML += html;
+
+}
+console.log(todoListHTML);
+
+document.querySelector('.js-todo-list')
+.innerHTML = todoListHTML;
+}
+
 function addTdo(){
 
 const inputElement = document.querySelector('.js-name-input');
@@ -6,5 +24,6 @@ const name = inputElement.value;
 todoList.push(name);
 console.log(todoList);
 inputElement.value = '';
+renderTdoList();
 
 }
