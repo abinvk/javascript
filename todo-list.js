029448e4320179d15bb2da0017ca876b76cc5de3
Abin vk <vettikkadanabin@gmail.com>
2025-12-09 12:17:10 +0530

    javascript

--- todo-list.js
+++ todo-list.js
@@ -16,13 +16,16 @@ for(let i = 0; i<todoList.length; i++){
     //const name = todoObject.name;
     //const dueDate = todoObject.dueDate;
     const { name, dueDate } = todoObject;
-    const html = `<p>${name} ${dueDate}
-    <button onclick="
-    todoList.splice(${i},1);
-    renderTdoList();
-    ">Delete</button></P>`;
-    todoListHTML += html;
 
+  const html =`
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+      todoList.splice(${i},1);
+      renderTdoList();
+    "class="delete-todo-button">Delete</button>
+    `;
+    todoListHTML += html;
 }
 
 
