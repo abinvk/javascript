8e88da61b2e02e5edd04e70d9472e58492ac6f06
Abin vk <vettikkadanabin@gmail.com>
2025-12-09 09:33:12 +0530

    javascript

--- todo-list.js
+++ todo-list.js
@@ -1,4 +1,10 @@
-const todoList = ['make dinner', 'wash dishes'];
+const todoList = [{
+    name:'make dinner',
+    dueDate: '2024-06-30'
+   },{
+     name:'wash dishes',
+     dueDate: '2024-06-25'
+   }];
 
 renderTdoList();
 
@@ -6,8 +12,15 @@ function renderTdoList () {
 
 let todoListHTML = '';
 for(let i = 0; i<todoList.length; i++){
-    const todo =todoList[i];
-    const html = `<p>${todo}</P>`;
+    const todoObject =todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+    const html = `<p>${todo}
+    <button onclick="
+    todoList.splice(${i},1);
+    renderTdoList();
+    ">Delete</button></P>`;
     todoListHTML += html;
 
 }
