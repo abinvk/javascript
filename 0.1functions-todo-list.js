59c6fb0fb39e135e8ca630d051b78fae601f3fbd
Abin vk <vettikkadanabin@gmail.com>
2025-12-13 11:35:12 +0530

    javascript

---
+++ 0.1functions-todo-list.js
@@ -0,0 +1,53 @@
+
+const todoList = [{
+    name:'make dinner',
+    dueDate: '2024-06-30'
+   },{
+     name:'wash dishes',
+     dueDate: '2024-06-25'
+   }];
+
+renderTdoList();
+
+function renderTdoList () {
+
+let todoListHTML = '';
+for(let i = 0; i<todoList.length; i++){
+    const todoObject =todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+
+  const html =`
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+      todoList.splice(${i},1);
+      renderTdoList();
+    "class="delete-todo-button">Delete</button>
+    `;
+    todoListHTML += html;
+}
+
+
+document.querySelector('.js-todo-list')
+.innerHTML = todoListHTML;
+}
+
+function addTdo(){
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+
+const inputElement = document.querySelector('.js-name-input');
+const name = inputElement.value;
+todoList.push({
+    //name:name,
+    //dueDate: dueDate,
+    name,
+    dueDate
+} );
+inputElement.value = '';
+renderTdoList();
+
+}
+     
